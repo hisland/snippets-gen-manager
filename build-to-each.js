29a4a491dce343483650451a9bb161045c81341f
hisland <hisland@qq.com>
2018-10-07 14:38:23 +0800

    can from,to atom

--- build-to-each.js
+++ build-to-each.js
@@ -1,12 +1,64 @@
+const fs = require('fs')
 const cson = require('cson')
-const _ = require('lodash')
+const { forEach, map } = require('lodash')
 
-// const db = require('./db.js');
+// toAtom()
+// fromAtom()
 
-const list1 = cson.load('./db.cson')
+function toAtom() {
+  const list1 = cson.load('./db-raw.cson')
+  // console.log(list1)
+  const rs1 = {}
+  list1.forEach(({ scopeList, snippetList }) => {
+    scopeList.forEach(scope => {
+      snippetList.forEach(({ name, trigger, content, desc }) => {
+        rs1[scope] = rs1[scope] || {}
+        rs1[scope][name] = {
+          prefix: trigger,
+          body: content,
+        }
+      })
+    })
+  })
+  const rs2 = cson.createCSONString(rs1, {
+    indent: '  ',
+  })
+  // fs.writeFileSync('db-raw-to-atom.cson', rs2)
+  fs.writeFileSync('/Users/hisland/.atom/snippets.cson', rs2)
+}
 
-console.log(list1)
+function fromAtom() {
+  const atomOrg = cson.load('/Users/hisland/.atom/snippets.cson')
+  // const rs1 = []
+  // forEach(atomOrg, (vv1, scope) => {
+  //   const oneScope = {
+  //     scopeList: [scope],
+  //     snippetList: [],
+  //   }
+  //   oneScope.snippetList = map(vv1, ({ prefix, body }, name) => ({
+  //     name,
+  //     trigger: prefix,
+  //     content: body,
+  //     desc: '',
+  //   }))
+  //
+  //   rs1.push(oneScope)
+  // })
+  const rs1 = map(atomOrg, (vv1, scope) => ({
+    scopeList: [scope],
+    snippetList: map(vv1, ({ prefix, body }, name) => ({
+      name,
+      trigger: prefix,
+      content: body,
+      desc: '',
+    })),
+  }))
+  const rs2 = cson.createCSONString(rs1, {
+    indent: '  ',
+  })
+  fs.writeFileSync('db-from-atom.cson', rs2)
+}
 
-function toAtom() {}
 function toVSCode() {}
+
 function toSublimeText3() {}
