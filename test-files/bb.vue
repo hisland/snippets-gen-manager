ec6c7c65066886776e395d9c6f510fdb15592985
hisland <hisland@qq.com>
2019-03-21 11:05:01 +0800

    add test files

---
+++ test-files/bb.vue
@@ -0,0 +1,13 @@
+<style lang="scss" scoped>
+</style>
+<script>
+export default {};
+</script>
+<template>
+  <div></div>
+</template>
+<template lang="pug">
+div
+  div(v-if="")
+  template(v-if="")
+</template>
